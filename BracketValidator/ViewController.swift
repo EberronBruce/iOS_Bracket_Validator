8f016ac8645e0cc613ad4186b657c0ed76b8caf8
Bruce <bruce@redravencomputing.com>
2022-07-11 17:04:36 -0600

    Initial Commit

---
+++ BracketValidator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BracketValidator
+//
+//  Created by Bruce Burgess on 7/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
